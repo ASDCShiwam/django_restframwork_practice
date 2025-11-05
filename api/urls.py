36d053a9153f838ead125e70c064ffd79b791524
ASDCShiwam <developershiwam1569@gmail.com>
2025-11-06 02:14:07 +0530

    class based view

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('students/',views.studentsViews),
     path('students/<int:pk>/',views.studentDetailViews),
+    path('employees/',views.employee.as_view()),
+    path('employees/<int:pk>/',views.employeeDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -5,6 +5,10 @@ from .serializers import StudentsSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from employee.models import Employee
+from .serializers import EmployeeSerializer
+from django.http import Http404
 
 # Create your views here.
 @api_view(['GET','POST'])
@@ -40,3 +44,30 @@ def studentDetailViews(request,pk):
     elif request.method=='DELETE':
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class employee(APIView):
+    def get(self,request):
+        emp=Employee.objects.all()
+        serializer=EmployeeSerializer(emp,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)  
+    def post(self,request):
+        serializer=EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+class employeeDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Employee.objects.get(id=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+    def get(self,request,pk):
+        emp=self.get_object(pk)
+        serializer=EmployeeSerializer(emp)
+        return Response(serializer.data, status=status.HTTP_200_OK)  
+    
+           
+    
